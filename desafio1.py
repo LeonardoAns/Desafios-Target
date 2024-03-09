10bc8d501c0b5e734648a728cf73a8d4af021594
leonardo anschau <leonardianschauq@gmail.com>
2024-03-09 20:01:00 -0300

    Desafios target

--- desafio1.py
+++ desafio1.py
@@ -1,17 +1,16 @@
 def fib(n):
-    Fib = [0,1]
-    while len(Fib) < n:
-        nextFib = Fib[-1] + Fib[-2]
-        Fib.append(nextFib)
-    return Fib
+    fib_list = [0,1]
+    while len(fib_list) < n:
+        nextFib = fib_list[-1] + fib_list[-2]
+        fib_list.append(nextFib)
+    return fib_list 
 
 def verificador(num):
     fib_sequence = fib(n)
     if num in fib_sequence:
         return "O número {} pertence a sequencia".format(num)
     else:
-         return "O número {} não  a sequencia".format(num)
+         return "O número {} não pertence a sequencia".format(num)
     
 n = int(input("Digite um número"))
-
 print(verificador(n))
